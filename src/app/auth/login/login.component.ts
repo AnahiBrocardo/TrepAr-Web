a6e767c8fe72189999ed6a28a61a24cec07018f2
AnahiBrocardo <anahibrocardo213@gmail.com>
2024-10-26 17:25:54 -0300

    Creacion del reactive form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms'; //importamos la clase form builder
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit{
+  fb= inject(FormBuilder);// Inyecta el servicio FormBuilder utilizando inject
 
-    constructor() {
-      console.log('LoginComponent loaded!');
-    }
+  loginForm: FormGroup = this.fb.group({//loginForm: FormGroup ->define una propiedad loginForm de tipo FormGroup.
+    //this.fb.group(): Usa FormBuilder para crear un grupo de controles de formulario con distintos campos
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+
+  ngOnInit(): void {//Método que se ejecuta después de la creación del componente.
   
-    ngOnInit(): void {
-      console.log('LoginComponent initialized!');
-    }
   }
+}
