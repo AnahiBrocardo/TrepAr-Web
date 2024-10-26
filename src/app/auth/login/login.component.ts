ac2b909a502bd4053eedd91963ac5cc274e45d61
AnahiBrocardo <anahibrocardo213@gmail.com>
2024-10-26 17:28:51 -0300

    Comentarios de las validaciones del form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -14,11 +14,19 @@ export class LoginComponent implements OnInit{
 
   loginForm: FormGroup = this.fb.group({//loginForm: FormGroup ->define una propiedad loginForm de tipo FormGroup.
     //this.fb.group(): Usa FormBuilder para crear un grupo de controles de formulario con distintos campos
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required],
+    
+    email: ['', [Validators.required, Validators.email]], // Validaciones: campo 'email' obligatorio y debe tener un formato de correo electrónico válido
+    password: ['', Validators.required],// validacion campo 'password' obligatorio
   });
 
   ngOnInit(): void {//Método que se ejecuta después de la creación del componente.
   
   }
+
+  //metodo login, que se va a llamar cuando el usuario presione el boton de iniciar sesion
+  login(){
+
+  }
+
+  
 }
