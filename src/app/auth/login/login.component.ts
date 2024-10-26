62e2ef0757ed27380cb6bd5d9c6e474d0d5d97b4
AnahiBrocardo <anahibrocardo213@gmail.com>
2024-10-26 17:51:50 -0300

    Commit validaciones login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms'; //importamos la clase form builder
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,14 +11,17 @@ import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit{
-  fb= inject(FormBuilder);// Inyecta el servicio FormBuilder utilizando inject
+ loginForm: FormGroup;
 
-  loginForm: FormGroup = this.fb.group({//loginForm: FormGroup ->define una propiedad loginForm de tipo FormGroup.
-    //this.fb.group(): Usa FormBuilder para crear un grupo de controles de formulario con distintos campos
-    
-    email: ['', [Validators.required, Validators.email]], // Validaciones: campo 'email' obligatorio y debe tener un formato de correo electrónico válido
-    password: ['', Validators.required],// validacion campo 'password' obligatorio
-  });
+  constructor(private formBuilder: FormBuilder, private router: Router){
+   this.loginForm= this.formBuilder.group({//loginForm: FormGroup ->define una propiedad loginForm de tipo FormGroup.
+      //this.fb.group(): Usa FormBuilder para crear un grupo de controles de formulario con distintos campos
+      
+      email: ['', [Validators.required, Validators.email]], // Validaciones: campo 'email' obligatorio y debe tener un formato de correo electrónico válido
+      password: ['', Validators.required],// validacion campo 'password' obligatorio
+    });
+  }
+ 
 
   ngOnInit(): void {//Método que se ejecuta después de la creación del componente.
   
@@ -25,7 +29,14 @@ export class LoginComponent implements OnInit{
 
   //metodo login, que se va a llamar cuando el usuario presione el boton de iniciar sesion
   login(){
-
+    if(this.loginForm.valid){ //si el formulario es valido
+    console.log("Llamar al servicio de login");
+    this.router.navigateByUrl('/dashboard');// Redirige al usuario a la ruta '/dashboard' utilizando el método navigateByUrl
+    this.loginForm.reset;//reseteamos los campos del form
+    }else{
+      this.loginForm.markAllAsTouched();// Marca todos los controles del formulario como 'touched', es decir, se considera que el usuario ha interactuado con ellos.
+    alert("Error al ingresar los datos");
+  }
   }
 
   
