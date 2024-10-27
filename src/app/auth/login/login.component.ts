53fd8018e5d22f4ca43178853f1ae2b868b1ed21
AnahiBrocardo <anahibrocardo213@gmail.com>
2024-10-26 23:21:01 -0300

    Comentarios y manejo de nulos en campos

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms'; //importamos la clase form builder
+import {AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms'; //importamos la clase form builder
 import { Router } from '@angular/router';
 
 @Component({
@@ -27,13 +27,29 @@ export class LoginComponent implements OnInit{
   
   }
 
+
+  get email(): AbstractControl | null {//siempre puede acceder a cualquier control de formulario a través del metodo get 
+    return this.loginForm.get('email');//Devuelve el control email si existe o null
+  }
+
+  /*AbstractControl | null ->es el tipo de retorno, esta función puede devolver un objeto de tipo AbstractControl
+   o null. AbstractControl es una clase de Angular que representa un control en un formulario reactivo  */
+  
+  get password(): AbstractControl | null {
+    return this.loginForm.get('password');
+  }
+
   //metodo login, que se va a llamar cuando el usuario presione el boton de iniciar sesion
   login(){
+    
     if(this.loginForm.valid){ //si el formulario es valido
+
     console.log("Llamar al servicio de login");
     this.router.navigateByUrl('/dashboard');// Redirige al usuario a la ruta '/dashboard' utilizando el método navigateByUrl
-    this.loginForm.reset;//reseteamos los campos del form
-    }else{
+    this.loginForm.reset();//reseteamos los campos del form
+    
+  }else{
+
       this.loginForm.markAllAsTouched();// Marca todos los controles del formulario como 'touched', es decir, se considera que el usuario ha interactuado con ellos.
     alert("Error al ingresar los datos");
   }
