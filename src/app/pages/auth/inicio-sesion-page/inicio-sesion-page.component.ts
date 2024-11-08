661eceb5aed7f9ec8e20960275433b23352a1a19
AnahiBrocardo <anahibrocardo213@gmail.com>
2024-11-08 10:47:24 -0300

    Creacion inicioSesionPage y registarsePage

---
+++ src/app/pages/auth/inicio-sesion-page/inicio-sesion-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio-sesion-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './inicio-sesion-page.component.html',
+  styleUrl: './inicio-sesion-page.component.css'
+})
+export class InicioSesionPageComponent {
+
+}
---
+++ src/app/pages/auth/registrarse-page/registrarse-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrarse-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './registrarse-page.component.html',
+  styleUrl: './registrarse-page.component.css'
+})
+export class RegistrarsePageComponent {
+
+}
