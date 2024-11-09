d3c1e3e07ff4398890a0fc83be7199ecf6af21e4
Julieta Alijas <108241455+JulietaAlijas@users.noreply.github.com>
2024-11-09 16:29:42 -0300

    Paginado

--- src/app/componentes/simulador/funciones/listar-simulador/listar-simulador.component.ts
+++ src/app/componentes/simulador/funciones/listar-simulador/listar-simulador.component.ts
@@ -9,8 +9,10 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatTableModule} from '@angular/material/table';
-import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-listar-simulador',
   standalone: true,
@@ -20,7 +22,9 @@ import {MatTableDataSource} from '@angular/material/table';
     MatInputModule, 
     MatFormFieldModule, 
     MatTableModule,
-    MatPaginatorModule],
+    MatPaginatorModule,
+    FormsModule
+    ],
   templateUrl: './listar-simulador.component.html',
   styleUrl: './listar-simulador.component.scss'
 })
@@ -37,7 +41,8 @@ listarTodasSimulaciones(){
   this.SimuladorService.getSimulador().subscribe(
     {
       next: (Simulador: Simulador[]) =>{
-        this.listaSimulacions = Simulador
+        this.listaSimulacions = Simulador;
+        this.dataSource.data = Simulador;
       }, error: (e:Error)=>{
         console.log(e.message);
       }
@@ -67,12 +72,41 @@ displayedColumns: string[] = ['id', 'nombre', 'PrecioFinal', 'accions'];
 dataSource = new MatTableDataSource<Simulador>([]);
 constructor() { this.leerTodo(); }
 
-leerTodo() {
-   this.SimuladorService.getSimulador().subscribe( 
-    (respuesta: any) => { 
-      this.dataSource.data = respuesta; // Ajuste según la estructura de tu respuesta 
-    },
-    (error) => { console.error('Error al obtener los simuladores:', error); } ); }
+leerTodo() { 
+  const inicio = this.numeroDePag * this.cantidadPorPagina; 
+  const fin = inicio + this.cantidadPorPagina;
+
+  this.SimuladorService.getSimulador().subscribe({
+    next: (simuladores: Simulador[]) =>{
+      let datosFiltrados = simuladores;
+
+      // Filtrar por texto buscado 
+      if (this.textoBuscado.trim() !== '') { 
+        datosFiltrados = simuladores.filter(simulador =>
+          simulador.nombre.toLowerCase().includes(this.textoBuscado.toLowerCase()) ); }
+
+      this.cantidadTotal = datosFiltrados.length; // Ajuste según la estructura de tu respuesta 
+      this.dataSource.data = datosFiltrados.slice(inicio, fin);// Suponiendo que la cantidad total es la longitud del array de simuladores 
+       }, 
+    error: (error) => { 
+      console.error('Error al obtener los simuladores:', error); } }); }
+
+///--------------paginado---------------------------
+cantidadTotal= 0;
+cantidadPorPagina= 10;
+numeroDePag= 0;
+opcionesDePaginado: number[] = [1 , 5, 10 , 20 , 50];
+
+CambiarPagina(event: any){
+  this.cantidadPorPagina = event.pageSize; 
+  this.numeroDePag= event.pageIndex;
+  this.leerTodo(); //esto no se si esta bien
+
+}
+///--------------Busqueda---------------------------
+textoBuscado= '';
+
+
 
 
 }
