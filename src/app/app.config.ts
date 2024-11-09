1e2a55a8d0a539b9eda205f4ba1bd82e76799b3e
Julieta Alijas <108241455+JulietaAlijas@users.noreply.github.com>
2024-11-09 19:44:06 -0300

    Simulador

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,17 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-<<<<<<< HEAD
-=======
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
->>>>>>> S.D.Costos
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-<<<<<<< HEAD
-  provideHttpClient()]
-=======
-  provideHttpClient(), provideAnimationsAsync()]
->>>>>>> S.D.Costos
+    provideHttpClient(), 
+  provideAnimationsAsync()]
 };
