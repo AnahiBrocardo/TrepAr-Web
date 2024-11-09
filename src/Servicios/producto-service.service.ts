d2e6d9dc6a46fddd5a76aa0777ba8b28e7823788
Ayelen Ibañez <ibanezayelen307@gmail.com>
2024-11-08 22:33:20 -0300

    Creacion del servicio producto

---
+++ src/Servicios/producto-service.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductoInterface } from '../app/interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductoServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  urlBase: string = 'http://localhost:3000/productos'
+
+  getProductos(): Observable <ProductoInterface[]>{
+    return this.http.get<ProductoInterface[]>(this.urlBase)
+  }
+
+  postProductos(producto: ProductoInterface): Observable<ProductoInterface>{
+    return this.http.post<ProductoInterface>(this.urlBase, producto)
+  }
+
+  putProductos(producto: ProductoInterface, id: number): Observable<ProductoInterface> {
+    return this.http.put<ProductoInterface>(`${this.urlBase}/${id}`, producto)
+  }
+}
