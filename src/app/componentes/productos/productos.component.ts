abe5bac84452a42856a50dd4d2ee63da6c9ae937
Ayelen Ibañez <ibanezayelen307@gmail.com>
2024-11-09 23:04:11 -0300

    componente producto (componente)

--- src/app/componentes/productos/productos.component.ts
+++ src/app/componentes/productos/productos.component.ts
@@ -1,10 +1,14 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductoServiceService } from '../../Servicios/productos/productos-service.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ProductoInterface } from '../../Interfaces/producto-interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-productos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './productos.component.html',
   styleUrl: './productos.component.css'
 })
@@ -24,4 +28,46 @@ export class ProductosComponent implements OnInit {
    })
   }
 
+  fb= inject(FormBuilder)
+  productoService= inject(ProductoServiceService)
+
+  /*Contenidos de la barra desplegable */
+  categorias: string[] = ['Electrónica', 'Ropa', 'Hogar', 'Libros', 'Belleza', 'Juguetes', 'Deportes', 'Automotores', 'Alimentos', 'Mascotas', 'Otro']; 
+
+    /*No va a aceptar nincun campo que sea nulo con el nonnull.. */
+    formulario = this.fb.nonNullable.group(
+      {
+        id: ['', Validators.required],
+        nombre: ['', Validators.required],
+        categoria: ['', Validators.required],
+        descripcion: ['', Validators.required],
+        precio: ['', Validators.required]
+      }
+    )
+    
+    @Output()
+  emitirProducto: EventEmitter<ProductoInterface> = new EventEmitter(); 
+ 
+  addProducto()
+  {
+    if(this.formulario.invalid) return; 
+
+    const nproducto: ProductoInterface = this.formulario.getRawValue(); 
+    this.addProductoDB(nproducto)
+    this.emitirProducto.emit(nproducto); 
+  }
+
+  addProductoDB (producto: ProductoInterface){
+    this.productoService.postProductos(producto).subscribe(
+      {
+        next: (producto: ProductoInterface) => {
+          console.log(producto); 
+          alert('Tarea Guardada')
+        },
+        error: (e: Error) => {
+          console.log(e.message); 
+        }
+      }
+    )
+  }
 }
