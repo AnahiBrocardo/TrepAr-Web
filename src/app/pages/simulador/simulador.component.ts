338ea7b6a2768d98f40a4629be28c0e7054fd060
AnahiBrocardo <anahibrocardo213@gmail.com>
2024-11-09 21:48:41 -0300

    Import simulador component

--- src/app/pages/simulador/simulador.component.ts
+++ src/app/pages/simulador/simulador.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ListarSimuladorComponent } from '../../componentes/simulador/funciones/listar-simulador/listar-simulador.component';
 
 @Component({
   selector: 'app-simulador',
   standalone: true,
-  imports: [],
+  imports: [ListarSimuladorComponent],
   templateUrl: './simulador.component.html',
   styleUrl: './simulador.component.css'
 })
