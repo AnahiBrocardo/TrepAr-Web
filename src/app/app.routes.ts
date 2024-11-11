1828fc461d110427185c0332a4413dca978df856
AnahiBrocardo <anahibrocardo213@gmail.com>
2024-11-11 05:21:20 -0300

    proteccion rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,20 +10,23 @@ import { SettingsComponent } from './pages/settings/settings.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { InicioSesionPageComponent } from './pages/auth/inicio-sesion-page/inicio-sesion-page.component';
 import { RegistrarsePageComponent } from './pages/auth/registrarse-page/registrarse-page.component';
+import { loginGuard } from './guards/auth.guards';
+import { logOutGuard } from './guards/auth.guards.logOut';
 
 export const routes: Routes = [
-   { path: '', component: LandingPageComponent},
-    {path: 'acceso', component: InicioSesionPageComponent},
-    {path: 'registrarse', component: RegistrarsePageComponent},
+   { path: '', component: LandingPageComponent,canActivate:[logOutGuard]},
+    {path: 'acceso', component: InicioSesionPageComponent, canActivate:[logOutGuard]},
+    {path: 'registrarse', component: RegistrarsePageComponent,canActivate:[logOutGuard]},
     {
         path: 'dashboard/:id', 
         component: DashboardComponent,
         children: [
-            {path: 'principal', component: PrincipalComponent},
-            {path: 'producto/:id', component: ProductoComponent},
-            {path: 'simulador/:id', component: SimuladorComponent },
-            {path: 'settings/:id', component: SettingsComponent}
-        ]
+            {path: 'principal', component: PrincipalComponent, canActivate:[loginGuard]},
+            {path: 'producto/:id', component: ProductoComponent,canActivate:[loginGuard]},
+            {path: 'simulador/:id', component: SimuladorComponent,canActivate:[loginGuard] },
+            {path: 'settings/:id', component: SettingsComponent,canActivate:[loginGuard]}
+        ],
+        canActivate:[loginGuard] //si estoy loggeado puedo acceder a esta ruta
     }, 
    {path: '**', redirectTo: '', pathMatch: 'full'} //si pone cualquier otra cosa lo redirige a vacio '', osea a la landing
 ];
