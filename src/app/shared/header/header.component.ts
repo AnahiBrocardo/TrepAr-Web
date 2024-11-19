7882961c763a00549e5c2ae8878e15f98bfbc3c3
AnahiBrocardo <anahibrocardo213@gmail.com>
2024-11-18 21:19:31 -0300

    cambios en el header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isHome = false;
 
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isHome = this.router.url === '/'; }
+    });
+  }
+
+  ngOnInit() {
+    console.log(this.router.url); // Para verificar el valor de la URL
+    return this.router.url === '/' || this.router.url === '';
+  }
 }
