47467e1add0ef98d551fd4113c54a3b05885c8cb
AnahiBrocardo <anahibrocardo213@gmail.com>
2024-11-20 08:58:47 -0300

    Cambio alerta registrarse

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -240,13 +240,22 @@ togglePasswordVisibility() {
         };
 
         this.perfilService.agregarPerfil(newProfile).subscribe({
-          next: () => {
-            Swal.fire({
-              icon: 'success',
-              title: 'Registro exitoso',
-              text: 'Tu cuenta ha sido creada exitosamente.',
-            });
-            this.router.navigate(['/acceso']);
+          next: () => {const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            }
+          });
+          Toast.fire({
+            icon: "success",
+            title: "Registro exitoso"
+          });
+          this.router.navigate(['/acceso']);
           },
           error: (error) => {
             console.error('Error al guardar el perfil', error);
